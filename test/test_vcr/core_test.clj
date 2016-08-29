9ac802cfe55a2d08bbcd502a20e52d03d20f9b98
Matthew Boston <matthew@matthewboston.com>
2016-08-29 17:04:02 -0600

    use vcr-clj to cache HTTP responses

--- test/test_vcr/core_test.clj
+++ test/test_vcr/core_test.clj
@@ -1,7 +1,15 @@
 (ns test-vcr.core-test
   (:require [clojure.test :refer :all]
-            [test-vcr.core :refer :all]))
+            [test-vcr.core :refer :all]
+            [vcr-clj.clj-http :refer [with-cassette]]))
 
 (deftest user-info-test
   (testing "get user info"
-    (is (not (nil? (user-info "bostonaholic"))))))
+    (testing "without cassette"
+      (println "without cassette")
+      (is (not (nil? (time (user-info "bostonaholic"))))))
+
+    (testing "with cassette"
+      (with-cassette ::bostonaholic-user-info
+        (println "with cassette")
+        (is (not (nil? (time (user-info "bostonaholic")))))))))
